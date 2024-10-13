539a3b71931afae644916597880c8483e40fa719
First-project <mehedihasan0937580@gmail.com>
2024-10-13 16:12:45 +0600

    probleSolution & button code upadte. All done

--- src/components/Home/ProblemSolution.jsx
+++ src/components/Home/ProblemSolution.jsx
@@ -1,15 +1,22 @@
+import { useState } from 'react'
 import { ProblemSolution } from '../lib/db'
 import Container from './../common/container'
 import ProblemSolutionCard from './ProblemSolutionCard'
 import ProblemSolutionHeader from './ProblemSolutionHeader'
 
 const ProblemSolutions = () => {
+  const [isActive, setActive] = useState(1)
   return (
     <Container className="max-w-[1182px] p-6 mt-10">
       <ProblemSolutionHeader />
       <div className="my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 place-items-center gap-2">
         {ProblemSolution.map((props, i) => (
-          <ProblemSolutionCard key={i} {...props} />
+          <ProblemSolutionCard
+            key={i}
+            onClick={() => setActive(i)}
+            isActive={isActive === i}
+            {...props}
+          />
         ))}
       </div>
     </Container>
--- src/components/Home/ProblemSolutionCard.jsx
+++ src/components/Home/ProblemSolutionCard.jsx
@@ -1,26 +1,54 @@
-const ProblemSolutionCard = ({ img, title, des1, des2 }) => {
+import { cn } from '../lib/utils/cn'
+
+const ProblemSolutionCard = ({ img, title, des1, des2, onClick, isActive }) => {
   return (
     <>
-      <div className="w-[272px] h-[349px] flex gap-3 flex-col shadow-lg p-5 hover:bg-gradient-primary group hover:cursor-pointer transition-all duration-300 rounded-20">
-        <div className="flex flex-col gap-3">
-          <div className="space-y-3">
-            <img
-              src={img}
-              alt={title}
-              className="bg-gradient-primary rounded-full h-[58px] w-[58px]"
-            />
-            <h3 className="group-hover:text-white text-secondary font-bold font-nunito-sans text-[18px] leading-6 ">
-              {title}
-            </h3>
-          </div>
-          <div className="space-y-3">
-            <p className="group-hover:text-white text-accent">{des1}</p>
-            {des2 && (
-              <p className="group-hover:text-white text-accent">{des2}</p>
-            )}
+      <button type="button" className="text-start" onClick={onClick}>
+        <div
+          className={cn(
+            'w-[272px] h-[349px] flex gap-3 flex-col shadow-lg p-5 hover:bg-gradient-primary group hover:cursor-pointer transition-all duration-300 rounded-20',
+            isActive && 'bg-gradient-primary'
+          )}
+        >
+          <div className="flex flex-col gap-3">
+            <div className="space-y-3">
+              <img
+                src={img}
+                alt={title}
+                className="bg-gradient-primary rounded-full h-[58px] w-[58px]"
+              />
+              <h3
+                className={cn(
+                  'group-hover:text-white text-secondary font-bold font-nunito-sans text-[18px] leading-6',
+                  isActive && 'text-white'
+                )}
+              >
+                {title}
+              </h3>
+            </div>
+            <div className="space-y-3">
+              <p
+                className={cn(
+                  'group-hover:text-white text-accent',
+                  isActive && 'text-white'
+                )}
+              >
+                {des1}
+              </p>
+              {des2 && (
+                <p
+                  className={cn(
+                    'group-hover:text-white text-accent',
+                    isActive && 'text-white'
+                  )}
+                >
+                  {des2}
+                </p>
+              )}
+            </div>
           </div>
         </div>
-      </div>
+      </button>
     </>
   )
 }
